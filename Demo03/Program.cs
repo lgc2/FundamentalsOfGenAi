9e6a74d2d72d04563051d1eb1c9df9b149247436
lgc2 <lgustavoccarvalho@gmail.com>
2025-11-23 11:51:42 -0300

    create Demo03 project

---
+++ Demo03/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+
+var builder = Kernel.CreateBuilder()
+	.AddOllamaChatCompletion(modelId: "phi3:3.8b", endpoint: new Uri("http://localhost:11434/"));
+
+builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+var kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+OllamaPromptExecutionSettings settings = new()
+{
+	FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+var history = new ChatHistory();
+
+string? prompt;
+do
+{
+	Console.Write("User > ");
+	prompt = Console.ReadLine();
+
+	history.AddUserMessage(prompt);
+
+	var result = await chatCompletionService.GetChatMessageContentAsync(history, executionSettings: settings, kernel: kernel);
+
+	Console.WriteLine("Assistant > " + result);
+
+	history.AddMessage(result.Role, result.Content ?? string.Empty);
+}
+while (prompt is not null);
