53953c5a3754179ca2c76e3d7dab864f05d15d1f
lgc2 <lgustavoccarvalho@gmail.com>
2025-12-23 09:37:41 -0300

    feat(embeddings): create demos 04 and 05

---
+++ Demo04/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Embeddings;
+using OllamaSharp;
+
+//var services = new ServiceCollection();
+//var serviceProvider = services.BuildServiceProvider();
+
+//services.AddOllamaEmbeddingGenerator(
+//	modelId: "mxbai-embed-large",
+//	endpoint: new Uri("http://localhost:11434"),
+//	serviceId: "OllamaApiClient");
+
+//services.AddTransient(x => new Kernel(x));
+
+//using var scope = serviceProvider.CreateScope();
+
+var ollamaClient = new OllamaApiClient(uriString: "http://localhost:11434", defaultModel: "mxbai-embed-large");
+
+var embeddingService = ollamaClient.AsTextEmbeddingGenerationService();
+var embeddings = await embeddingService.GenerateEmbeddingsAsync(["exemplo de texto 1", "exemplo de texto 2"]);
