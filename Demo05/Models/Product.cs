009fbcaf05c5faf4c83c6b3347aac58192b7b7ee
lgc2 <lgustavoccarvalho@gmail.com>
2025-12-23 11:44:31 -0300

    feat(embeddings): transform demo05 into web application, implement EF mappings, add models and implement seed data endpoint

---
+++ Demo05/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo05.Models;
+
+public class Product
+{
+	public int Id { get; set; }
+	public string Title { get; set; } = string.Empty;
+	public string Category { get; set; } = string.Empty;
+	public string Summary { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+}
--- Demo05/Program.cs
+++ Demo05/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demo05.Data;
+using Demo05.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SemanticKernel.Embeddings;
+using OllamaSharp;
+
+var builder = WebApplication.CreateBuilder(args);
+var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+	x.UseNpgsql(cnnStr, p => p.UseVector());
+});
+
+builder.Services.AddTransient<OllamaApiClient>(x => new OllamaApiClient(
+	uriString: "http://localhost:11434", defaultModel: "mxbai-embed-large:335m"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("v1/seed", async (AppDbContext context, OllamaApiClient ollama) =>
+{
+	var products = await context.Products.AsNoTracking().ToListAsync();
+
+	foreach (var product in products)
+	{
+		var service = ollama.AsTextEmbeddingGenerationService();
+		var embeddings = await service.GenerateEmbeddingAsync(product.Category);
+
+		var recomendation = new Recomendation
+		{
+			Title = product.Title,
+			Category = product.Category,
+			Embedding = new Pgvector.Vector(embeddings)
+		};
+
+		await context.Recomendations.AddAsync(recomendation);
+		await context.SaveChangesAsync();
+	}
+
+	return Results.Ok(new { message = "OK" });
+});
+
+app.Run();
